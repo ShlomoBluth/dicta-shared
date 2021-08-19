db2362b09ca6f258b9ebd1d9e9f3af864fc4c9a5
Shlomo Bluth <lomibluth@gmail.com>
2021-08-19 09:45:17 +0300

    add commit

--- shared.js
+++ shared.js
@@ -34,13 +34,3 @@ Cypress.Commands.add('visitpage',({url})=>{
     visitpage(0,0)
   })
 
-import addContext from 'mochawesome/addContext'
-
-Cypress.on("test:after:run", (test, runnable) => {
-    
-    let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.js.*', '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
-
-    addContext({ test }, videoUrl)
-});
