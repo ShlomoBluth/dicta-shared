a85213ae8b748c4453ce395d33b7b6a73ce45357
ShlomoBluth <lomibluth@gmail.com>
2024-10-20 08:50:25 +0300

    commit

--- shared.js
+++ shared.js
@@ -15,6 +15,7 @@ Cypress.Commands.add('screenSize',({size})=>{
   }
 })
 
+
 Cypress.Commands.add('visitpage',({url})=>{
   // cy.visit(url,{
   //   retryOnStatusCodeFailure: true,
@@ -34,15 +35,6 @@ Cypress.Commands.add('visitpage',({url})=>{
         Connection: "Keep-Alive"
       }
     })
-    // cy.document().then(function (doc) {
-    // doc.addEventListener('mouseover', () => {
-    //   setTimeout(function () {
-    //     doc.location.replace("https://synoptic.dicta.org.il/")
-    //   }, 15000)
-    // })
-    // cy.get(elem).contains(text).click()
-    //cy.get(elem).contains(text).click({force:true})
-  //})
     cy.wait('@webreq'+Attempts).then(()=>{
       cy.get('body').then($body=>{
         if($body.find('#home').length==0 && $body.find('[class*="main-content"]').length==0
